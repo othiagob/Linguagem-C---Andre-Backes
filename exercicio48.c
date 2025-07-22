cc614ff99c256ee1dd7b14cfa106944bb2017917
othiagob <git@github.com:othiagob/Linguagem-C.git>
2025-07-22 00:36:49 -0400

    att

---
+++ exercicio48.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char texto[30] = "Hello World!\n";
+    fputs(texto, stdout);
+
+    return 0;
+}
---
+++ exercicio49.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    printf("Digite uma string: ");
+    fgets(str1, 100, stdin);
+    strcpy(str2, str1);
+    printf("String 1: %s\n", str1);
+    printf("String 2: %s\n", str2);
+
+    return 0;
+}
